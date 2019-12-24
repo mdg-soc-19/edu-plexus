850c84cd1a2dcd5260c44646132f7cc1cac40ef4
rochakjain361 <rochak_r@iitr.ec.in>
2019-12-24 10:31:03 +0530

    4th Commit

--- app/src/main/java/com/example/mdg_soc_eduplexus/DiscussionForum.java
+++ app/src/main/java/com/example/mdg_soc_eduplexus/DiscussionForum.java
@@ -3,6 +3,10 @@ package com.example.mdg_soc_eduplexus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DiscussionForum extends AppCompatActivity {
 
